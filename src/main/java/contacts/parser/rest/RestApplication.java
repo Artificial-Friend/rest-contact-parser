ecf390de4c8854d71b55105ec885388f82758f5f
Alex <65593125+Artificial-Friend@users.noreply.github.com>
2021-03-03 21:01:10 +0200

    init

---
+++ src/main/java/contacts/parser/rest/RestApplication.java
@@ -0,0 +1,13 @@
+package contacts.parser.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApplication.class, args);
+    }
+
+}
